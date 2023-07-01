bfb83cd9dca123baf67385d968fc58bc7ef0cfd3
alevinas <alefv.97@gmail.com>
2023-06-30 21:10:25 -0300

    CORS para ambiente local

--- src/main/java/msUsers/app/EcoSwapApplication.java
+++ src/main/java/msUsers/app/EcoSwapApplication.java
@@ -3,7 +3,10 @@ package msUsers.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = {"msUsers.controllers, msUsers.domain.repositories"})
 @EntityScan(basePackages = "msUsers.domain.entities")
@@ -14,4 +17,13 @@ public class EcoSwapApplication {
 		SpringApplication.run(EcoSwapApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
