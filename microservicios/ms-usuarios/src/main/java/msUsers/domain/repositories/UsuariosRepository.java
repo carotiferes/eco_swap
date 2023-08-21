86135d54de13b680148a17df1a14519b3ee96f21
alevinas <alefv.97@gmail.com>
2023-08-21 20:20:44 -0300

    Me faltó subir un archivo

--- microservicios/ms-usuarios/src/main/java/msUsers/domain/repositories/UsuariosRepository.java
+++ microservicios/ms-usuarios/src/main/java/msUsers/domain/repositories/UsuariosRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.support.JpaRepositoryImplementati
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepositoryImplementation<Usuario, Long> {
+public interface UsuariosRepository extends JpaRepositoryImplementation<Usuario, Long> {
 
 }
