8d2da34f297d389334a4ec47b4edcec09a7149cd
Carolina Tiferes <ctiferes@tdi.com.ar>
2023-10-02 17:19:35 -0300

    trueques en publicaciones + restyle para ux

--- web/src/app/layout/header/header.component.ts
+++ web/src/app/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuModel } from 'src/app/models/menu.model';
 import { AuthService } from 'src/app/services/auth.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 const menuData = require('../../data/menu.json')
 
 @Component({
@@ -23,13 +24,22 @@ export class HeaderComponent {
 	isUserLoggedIn: boolean;
 	profileType: 'particular' | 'fundacion';
 
-	constructor(private router: Router, private auth: AuthService){
+	constructor(private router: Router, private auth: AuthService, private usuarioService: UsuarioService){
 		this.mainTabs = menuData.filter((item: MenuModel) => item.type == 'pages')
 		this.personalTabs = menuData.filter((item: MenuModel) => item.type == 'personal')
 		this.url = router.url;
 
 		this.isUserLoggedIn = auth.isUserLoggedIn
 		this.profileType = auth.isUserSwapper() ? 'particular' : 'fundacion';
+
+		if (this.isUserLoggedIn) {
+			usuarioService.getUserByID(auth.getUserID()).subscribe({
+				next: (res: any) => {
+					this.userData = res;
+					//console.log(this.userData);
+				}
+			})
+		}
 	}
 
 	goTo(path: string){
